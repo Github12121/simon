f6ce66d0232e43777dcb55c5d6a8d57f60dd3e1d
Github12121 <lukecmcaree@gmail.com>
2018-03-10 16:49:16 -0500

    simon.js put back on Webstorm

--- simon.js
+++ simon.js
@@ -1,28 +1,38 @@
 const random = require('random-number-generator');
+const figlet = require('figlet');
+const chalk = require('chalk');
 const MAX = 5;
 const readlineSync = require('readline-sync');
 var keys = ['r', 'g', 'b', 'y'];
 var targets = [];
 var playing = true;
-console.log('simon.js');
-console.log('A fun electronic version of Simon!');
-do {
-    targets.push(random(3));
-    console.log(targets);
-    for (var i = 0; i < targets.length; i++){
-        var guessNum = i + 1;
-        var guess = readlineSync.question('Guess #'+ guessNum +': ');
-        var target = targets[i];
-        if (guess == keys[target]) {
-            console.log('You\'re right!');
-        } else {
-            console.log('You\'re wrong!');
-            playing = false;
-        }
-    }
 
-    if (targets.length == MAX) {
-        playing = false;
-        console.log('Nice Job!');
-    }
-}while(playing);
+figlet('Simon',{
+    //isometric1
+    font: 'isometric1',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'}, function(err, data) {
+    console.log(chalk.blue.bgRed.bold(data));
+    console.log('A fun electronic version of Simon!');
+    do {
+        targets.push(random(3));
+        console.log(targets);
+        for (var i = 0; i < targets.length && true == playing; i++) {
+            var guessNum = i + 1;
+            var guess = readlineSync.question('Guess #' + guessNum + ': ');
+            var target = targets[i];
+            if (guess == keys[target]) {
+                console.log(chalk.white.bgGreen.bold('You\'re Right!'));
+            } else {
+                console.log(chalk.red.bold('You\'re Wrong!'));
+                console.log(chalk.white.bold('Game Over!'));
+                playing = false;
+            }
+        }
+
+        if (targets.length == MAX) {
+            playing = false;
+            console.log('Nice Job!');
+        }
+    } while (playing);
+});
