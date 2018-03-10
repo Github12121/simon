4d24b455f141181c41bc7cabeed70e76a3e1dc45
Github12121 <lukecmcaree@gmail.com>
2018-03-10 17:11:22 -0500

    changed font

--- simon.js
+++ simon.js
@@ -9,7 +9,7 @@ var playing = true;
 
 figlet('Simon',{
     //isometric1
-    font: 'ticks',
+    font: 'isometric1',
     horizontalLayout: 'default',
     verticalLayout: 'default'}, function(err, data) {
     console.log(chalk.blue.bgRed.bold(data));
