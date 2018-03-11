a55d51c731d10034fb98a01b768b5e36ecc5fe9b
Github12121 <lukecmcaree@gmail.com>
2018-03-11 11:34:25 -0400

    Made a guide

---
+++ simonhelp.js
@@ -0,0 +1,10 @@
+figlet('Simon Help', {
+    font: 'colossal',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'}, function(err, data2) {
+    console.log(chalk.red.bgBlue.bold(data2));
+    console.log('How to play');
+    console.log('If you\'re dealing with a number such as 10110,');
+    console.log('enter the first letter, then press the enter key and');
+    console.log('repeat these steps.');
+});
